0d41db098c3356e01fe5b66a75a7fc8e52712c09
xclv-ai <ceo@xclv.com>
2026-01-18 11:13:38 +0100

    new

--- services/geminiService.ts
+++
@@ -1,3 +0,0 @@
-
-// This file is obsolete and should be deleted from your repository.
-// The application logic is now self-contained within `index.html`.
--- sw.js
+++
@@ -1,3 +0,0 @@
-
-// This file is obsolete and should be deleted from your repository.
-// The service worker has been disabled to prevent caching errors.
